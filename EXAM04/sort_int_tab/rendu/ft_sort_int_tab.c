dfab2c539f96eb692b531e8899ea9bb7ddd125c3
julien <julien66140@hotmail.fr>
2025-01-01 17:22:01 +0100

    sort int tab

---
+++ EXAM04/sort_int_tab/rendu/ft_sort_int_tab.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/01 17:09:55 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2025/01/01 17:21:46 by sakamoto-42      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	int				tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[i])
+			{
+				tmp = tab[j];
+				tab[j] = tab[i];
+				tab[i] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+/*
+int	main(void)
+{
+	int	tab[] = { 1, 2, 5, 4, 5, 3};
+	int	i;
+	int	size;
+
+	i = 0;
+	size = sizeof(tab) / sizeof(int);
+	while (i < size)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
+	printf("\n");
+	sort_int_tab(tab, size);
+	i = 0;
+	while (i < size)
+	{
+		printf("%d", tab[i]);
+		i++;
+	}
+}
+*/
